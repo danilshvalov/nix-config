921b74ef6b1356a0a0db554a6abee004d86b03cb
Daniil Shvalov <shvalovich@gmail.com>
2023-06-17 14:09:13 +0300

    feat: add home-manager config

---
+++ home.nix
@@ -0,0 +1,144 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "danilshvalov";
+  home.homeDirectory = "/Users/danilshvalov";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.cmake
+    pkgs.clang-tools
+    pkgs.texlab
+    pkgs.sqlfluff
+    pkgs.git
+    pkgs.boost
+    pkgs.openssl
+    pkgs.openssl.dev
+    pkgs.curl
+    pkgs.curl.dev
+    pkgs.zlib
+    pkgs.iconv
+    pkgs.krb5
+    pkgs.gtest
+    pkgs.gbenchmark
+    pkgs.llvmPackages_16.libcxxClang
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/danilshvalov/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.overlays = [
+      (import (builtins.fetchTarball {
+               url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+               }))
+  (
+   import (let
+# NOTE: see https://github.com/nix-community/neovim-nightly-overlay/issues/176
+# rev = "master";
+       rev = "c57746e2b9e3b42c0be9d9fd1d765f245c3827b7";
+       in
+       builtins.fetchTarball {
+       url = "https://github.com/nix-community/neovim-nightly-overlay/archive/${rev}.tar.gz";
+       })
+  )
+  ];
+
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-unstable;
+  };
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "git"
+        "per-directory-history"
+      ];
+    };
+    shellAliases = {
+      v = "nvim";
+      hm = "home-manager";
+      lmk = "latexmk";
+      python = "python3";
+    };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+  };
+}
